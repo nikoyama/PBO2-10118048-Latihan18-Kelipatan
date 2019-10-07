747f9221117572d712ca4179612941aaee9db2bc
nikoyama <nikoyama32@gmail.com>
2019-10-07 19:18:23 +0700

    first commit

---
+++ src/pbo2/pkg10118048/latihan18/kelipatan/PBO210118048Latihan18Kelipatan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pbo2.pkg10118048.latihan18.kelipatan;
+
+/**
+ *
+ * @author NIX
+ * NAMA : Niko Yama
+ * KELAS : PBO2
+ * NIM : 10118048
+ * Deskripsi program : program ini berisi kelipatan 3,5
+ */
+public class PBO210118048Latihan18Kelipatan {
+    
+        public static void main(String[] args) {
+            for (double i = 3.5; i <= 35; i+= 3.5) {
+                System.out.println(i);
+            }
+        }
+}
+
+    
